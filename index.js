144c36254e3433af282d8272fd53f1b4d7a2b0e9
Henry Felipe <henrycalleraa@gmail.com>
2023-02-09 13:47:32 -0300

    add name video

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const app = express()
+const fs = require('fs');   
 const ytdl = require('ytdl-core')
 
-
-app.get('/' ,(req,res)=> {
+app.get('/:name/' ,(req,res)=> {
     const {url} = req.query
-    const date = Date.now()
     res.header("Content-Disposition",'filter: "videoandaudio"', 'attachmentt: filename="video.mp4"')
-    return ytdl(url,{ filter: "videoandaudio"}).pipe(res);
+    return ytdl(url,{ filter: "videoandaudio"}).pipe(fs.createWriteStream(`./videos/${req.params.name}.mp4`));
 
-})
-
-
-
-app.listen(3000)
+})  
+app.listen(4000)
